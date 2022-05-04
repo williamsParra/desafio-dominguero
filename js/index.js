9fbb719bfad76747037ad9dcc27f95087db550c0
williams parrra <williams.parra.parra@gmail.com>
2022-05-03 22:03:37 -0400

    actualizacion de requerimientos
    se actualiza codigo de navegacion segun nuevos requerimientos
    los nodos de nivel de profundidad deben ser utilizar scroll
    
    el scroll se debe mover segun corresponda

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 //esperar la carga de la pagina
 window.onload = () => {
+  window.onscroll = null;
   //extraer campo de juego
-  const playLand = document.getElementsByClassName("contenedor");
+  const playLand = document.getElementsByClassName("container");
   //inicializar navegacion
   let person = {
     y: 0,
@@ -9,17 +10,21 @@ window.onload = () => {
   };
 
   window.addEventListener("keydown", (e) => {
-    if (e.code == "ArrowUp") {
+    e.preventDefault();
+
+    if (e.code == "ArrowUp" || e.code == "KeyW") {
       person = drawTable(playLand, person, 0, -1);
     }
-    if (e.code == "ArrowDown") {
+    if (e.code == "ArrowDown" || e.code == "KeyS") {
       person = drawTable(playLand, person, 0, 1);
     }
-    if (e.code == "ArrowLeft") {
+    if (e.code == "ArrowLeft" || e.code == "KeyA") {
       person = drawTable(playLand, person, -1, 0);
+      scrollMovin(playLand, person, -1);
     }
-    if (e.code == "ArrowRight") {
+    if (e.code == "ArrowRight" || e.code == "KeyD") {
       person = drawTable(playLand, person, 1, 0);
+      scrollMovin(playLand, person, 1);
     }
   });
 };
@@ -29,7 +34,7 @@ function drawTable(playLand, person, levelValue, deepValue) {
   let reference = { ...person };
 
   //quitar pintura de recuadro actual
-  playLand[reference.y].children[reference.x].classList.remove("activo");
+  playLand[reference.y].children[reference.x].classList.remove("active");
   //calcular maxima profundidad
   const maxDeep = playLand.length;
 
@@ -38,8 +43,8 @@ function drawTable(playLand, person, levelValue, deepValue) {
   reference.x += levelValue;
 
   //validar profundidad (direccion hacia abajo maxima)
-  if (person.y + deepValue > maxDeep-1) {
-    reference.y = maxDeep-1;
+  if (person.y + deepValue > maxDeep - 1) {
+    reference.y = maxDeep - 1;
   }
 
   //validar profundidad (direccion hacia arriba)
@@ -50,8 +55,8 @@ function drawTable(playLand, person, levelValue, deepValue) {
   const maxLevel = playLand[reference.y].children.length;
 
   //validar nivel (direccion hacia la derecha)
-  if (person.x + levelValue > maxLevel-1) {
-    reference.x = maxLevel-1;
+  if (person.x + levelValue > maxLevel - 1) {
+    reference.x = maxLevel - 1;
   }
 
   //validar nivel (direccion hacia la izquierda)
@@ -59,7 +64,19 @@ function drawTable(playLand, person, levelValue, deepValue) {
     reference.x = 0;
   }
 
-  playLand[reference.y].children[reference.x].classList.add("activo");
-
+  playLand[reference.y].children[reference.x].classList.add("active");
+  if(reference.y !=person.y){
+    scrollMovin(playLand,reference,levelValue);
+  }
   return reference;
+}
+
+function scrollMovin(playLand, person, direction) {
+  const maxLevel = playLand[person.y].children.length;
+  //sacar ancho de contenedor maestro y dividir por la cantidad de hijos
+  let widthScroll = playLand[person.y].offsetWidth / maxLevel;
+  if (person.x <= maxLevel / 2) {
+    //multiplicar ancho promedio x hijo activo y hacer direccion de scroll positiva o negativa segun dirección
+    playLand[person.y].scroll(widthScroll * person.x * direction, 0);
+  }
 }
